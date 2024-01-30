74f124f266eb1b649e6e7ad47b058492f3a8cb13
ntasnina <tasnina@vt.edu>
2024-01-30 19:31:58 +0000

    fixed main_icon.py

--- code/main_icon.py
+++ code/main_icon.py
@@ -6,15 +6,12 @@ from utils.common import *
 from train_icon_noise import Trainer
 
 def main(config_path: Union[Path, dict]):
-    time_start = time.time()
     for run_no in range(5):
         trainer = Trainer(config_path)
         print(trainer.params.gat_shapes)
         if trainer.params.mode=='train':
             trainer.train()
         trainer.forward(run_no) #save the model and features in run specific files.
-        time_end = time.time()
-        typer.echo(create_time_taken_string(time_start, time_end))
         print('Finished with run: ', run_no)
         del trainer
 
